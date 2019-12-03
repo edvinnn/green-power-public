3e7985105ae53d8b94d6ab83d65d2131c5f55183
Edvin Sladic <edvinnn@users.noreply.github.com>
2019-12-04 00:29:40 +0100

    updated description comment

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,7 @@
+/*
+    Handles all the updating of the components on the dashboard when a user is signed in. 
+    Hence this script should only be called from where a user can be signed in.
+*/
 (function($) {
     "use strict";
 
